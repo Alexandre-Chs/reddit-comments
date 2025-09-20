f721e03ca14b9d37e12b1fd2cd242c0c81998e1e
Alexandre Chs <oliuuum@gmail.com>
2025-09-20 11:40:28 +0200

    clear the frontend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,40 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const foo = 42;
-  if (foo == 42) {
-    console.log("loose equality");
-  }
-  const _bar = 123;
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex items-center justify-center h-screen text-4xl text-red-600">
+      hello world !!
+    </div>
   );
 }
 
