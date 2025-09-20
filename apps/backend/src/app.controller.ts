39f98bc1cf919b5e8baad1b342cf87a5e2341dd5
Alexandre Chs <oliuuum@gmail.com>
2025-09-20 16:55:28 +0200

    backend: setup prisma

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from '@prisma/client';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(): Promise<User[]> {
+    return this.appService.listUsers();
+  }
+
+  @Post()
+  createUser(): Promise<User> {
+    return this.appService.createTestUser();
   }
 }
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.server';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createTestUser(): Promise<User> {
+    return this.prisma.user.upsert({
+      where: { email: 'test.user@example.com' },
+      update: {},
+      create: {
+        email: 'test.user@example.com',
+        name: 'Test User',
+      },
+    });
+  }
+
+  async listUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
