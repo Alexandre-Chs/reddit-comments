b284c9aa840ee6e45894719207226ede3e1f9fbb
Alexandre Chs <oliuuum@gmail.com>
2025-10-05 15:57:32 +0200

    frontend: settings account info

--- apps/frontend/src/features/settings/components/SettingsAccount.tsx
+++ apps/frontend/src/features/settings/components/SettingsAccount.tsx
@@ -1,5 +1,23 @@
+import { useRouteContext } from "@tanstack/react-router";
+
 const SettingsAccount = () => {
-  return <div>SettingsAccount</div>;
+  const user = useRouteContext({ from: "/_app" });
+
+  return (
+    <div className="max-w-2xl space-y-12 py-6">
+      <div className="space-y-6">
+        <div>
+          <h3 className="text-lg font-medium">Username</h3>
+          <p className="text-sm text-muted-foreground">{user.username}</p>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-medium">Email</h3>
+          <p className="text-sm text-muted-foreground">{user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SettingsAccount;
