e3a8a2dee4779a411bbd6dd70f7f162ffc7da32d
Alexandre Chs <oliuuum@gmail.com>
2025-10-07 17:39:28 +0200

    backend: setup cron

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,10 +6,12 @@ import { AuthController } from './auth/auth.controller';
 import { PrismaService } from './prisma.server';
 import { AuthService } from './auth/auth.service';
 import { TeamsModule } from './teams/teams.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronService } from './cron/cron.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TeamsModule],
+  imports: [ConfigModule.forRoot(), TeamsModule, ScheduleModule.forRoot()],
   controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AuthService],
+  providers: [AppService, PrismaService, AuthService, CronService],
 })
 export class AppModule {}
